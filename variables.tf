b5fe785097a1b38f5470640a9a1db9d1298449d2
sachinmangalorkar <74595626+sachinmangalorkar@users.noreply.github.com>
2020-11-20 00:27:15 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+ description = "The location where resources will be created"
+ default = "East US"
+}
+
+variable "rg_network" {
+ description = "The name of the resource group in which the resources will be created"
+ default     = "Project01RG"
+}
+
+variable "admin_user" {
+   description = "User name to use as the admin account on the VMs that will be part of the VM Scale Set"
+   default     = "azureuser"
+}
+
+variable "admin_password" {
+   description = "Default password for admin account"
+   default = "Sp1jk3R!@12#"
+}
